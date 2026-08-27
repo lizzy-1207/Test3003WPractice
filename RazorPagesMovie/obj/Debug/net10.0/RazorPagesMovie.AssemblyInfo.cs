bb26e6b513af4fd4b26f05c3b340b17e601b81f8
mllliz007@myuct.ac.za <mllliz007@myuct.ac.za>
2026-08-27 15:15:57 +0200

    Updates1

--- RazorPagesMovie/obj/Debug/net10.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/obj/Debug/net10.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab149dfc7c4637789b73480d7a4384e9071fce5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2cf05dfd277d29457a3d7cb0ae4d93e8092f083")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
