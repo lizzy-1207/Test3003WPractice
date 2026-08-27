a037f5e9e4b1fa1031dbd77c840558a8998e2d21
mllliz007@myuct.ac.za <mllliz007@myuct.ac.za>
2026-08-27 16:36:10 +0200

    Update4

--- RazorPagesMovie/Pages/Privacy.cshtml.cs
+++ RazorPagesMovie/Pages/Privacy.cshtml.cs
@@ -5,6 +5,11 @@ namespace RazorPagesMovie.Pages;
 
 public class PrivacyModel : PageModel
 {
+    private readonly ILogger<PrivacyModel> _logger;
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
     public void OnGet()
     {
     }
